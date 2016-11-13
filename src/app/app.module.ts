9cbc4593b5b0e9881ec9afcc9fed50448ec617b0
Gustavo Vilas Bôas <gu.boas13@gmail.com>
2016-11-13 01:13:37 -0200

    Estrutura básica do projeto

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,10 +4,16 @@ import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
 
 import { AppComponent } from './app.component';
+import { LoginComponent } from './login/login.component';
+import { SignupComponent } from './signup/signup.component';
+import { ChatComponent } from './chat/chat.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginComponent,
+    SignupComponent,
+    ChatComponent
   ],
   imports: [
     BrowserModule,
